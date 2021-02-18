023bc17dfaefd9a6e0c81470e855b086be434cf4
LEI Robin <Robin.LEI@edenred.com>
2021-02-18 11:05:19 +0800

    添加index.acss

---
+++ README.md
@@ -0,0 +1,19 @@
+
+html---axml
+css---acss
+
+index > 下面的所有文件都需要是index命名的
+
+# zhifubaoXCX
+
+[支付宝小程序官方文档](https://mini.open.alipay.com/channel/miniIndex.htm)
+
+[开发文档](https://opendocs.alipay.com/mini/developer)
+
+[开发平台创建小程序](https://open.alipay.com/mini/dev/create)
+
+1. 创建小程序应用
+
+前端商城Id：2021002128610896
+
+view 替代 div
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,24 +5,31 @@ Page({
   },
   onReady() {
     // 页面加载完成
+    console.log('onReady')
   },
   onShow() {
     // 页面显示
+    console.log('onShow')
   },
   onHide() {
     // 页面隐藏
+    console.log('onHide')
   },
   onUnload() {
     // 页面被关闭
+    console.log('onUnload')
   },
   onTitleClick() {
     // 标题被点击
+    console.log('onTitleClick')
   },
   onPullDownRefresh() {
     // 页面被下拉
+    console.log('onPullDownRefresh')
   },
   onReachBottom() {
     // 页面被拉到底部
+    console.log('onReachBottom')
   },
   onShareAppMessage() {
     // 返回自定义分享信息
