4737bc9da9f6b5f5d37f56fdc76cfa4e28a817dc
LEI Robin <Robin.LEI@edenred.com>
2021-02-26 13:52:37 +0800

    首页静态页面编写完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -61,7 +61,41 @@ Page({
           id: 6
         }
       ]
-    }
+    },
+    new_goods_list: [
+      {
+        img_url: '/resources/images/1001.png',
+        id: 1,
+        tags: [
+          {tagName: '套装', tag_id: 1},
+          {tagName: '包包', tag_id: 2},
+          {tagName: '蔻驰', tag_id: 3}
+        ],
+        price: 2500.00,
+        likes: 33
+      },
+      {
+        img_url: '/resources/images/1002.png',
+        id: 2,
+        tags: [
+          {tagName: '礼盒', tag_id: 1},
+          {tagName: '兰蔻套装', tag_id: 2},
+          {tagName: '送礼', tag_id: 3},
+          {tagName: '高端产品', tag_id: 4},
+          {tagName: '透明水晶', tag_id: 5},
+          {tagName: '晶莹剔透', tag_id: 6}
+        ],
+        price: 500.00,
+        likes: 2
+      },
+      {
+        img_url: '/resources/images/1001.png',
+        id: 3,
+        tags: [],
+        price: 2500.00,
+        likes: 133
+      }
+    ]
   },
   onShow() {
     console.log('se')
