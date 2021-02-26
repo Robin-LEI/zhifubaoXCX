d4c3534d52a8c8cb072b513aa328a2d6867baf8c
LEI Robin <Robin.LEI@edenred.com>
2021-02-26 10:04:03 +0800

    首页轮播和类型介绍完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,38 @@
 Page({
+  data: {
+    swiperList: [
+      {
+        img_url: '/resources/images/home_swiper_1.png',
+        id: 1
+      },
+      {
+        img_url: '/resources/images/home_swiper_2.png',
+        id: 2
+      }
+    ],
+    typeIntroList: [
+      {
+        icon: '/resources/images/fang.png',
+        text: '全场包邮',
+        id: 1
+      },
+      {
+        icon: '/resources/images/flash.png',
+        text: '先行赔付',
+        id: 2
+      },
+      {
+        icon: '/resources/images/yuan.png',
+        text: '7天无理由',
+        id: 3
+      },
+      {
+        icon: '/resources/images/ling.png',
+        text: '退货先补',
+        id: 4
+      }
+    ]
+  },
   onShow() {
     console.log('se')
   }
