1ef04b40d1af8dc5a387b84297b1adbef834db95
LEI Robin <Robin.LEI@edenred.com>
2021-03-02 16:06:47 +0800

    使用ajax替换

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,6 @@
 Page({
   data: {
-    swiperList: [
-      {
-        img_url: '/resources/images/home_swiper_1.png',
-        id: 1
-      },
-      {
-        img_url: '/resources/images/home_swiper_2.png',
-        id: 2
-      }
-    ],
+    swiperList: [],
     typeIntroList: [
       {
         icon: '/resources/images/fang.png',
@@ -33,71 +24,67 @@ Page({
       }
     ],
     reco_goods: {
-      title: '推荐商品',
-      icon: '/resources/images/rec.png',
-      list: [
-        {
-          img_url: '/resources/images/1001.png',
-          id: 1
-        },
-        {
-          img_url: '/resources/images/1002.png',
-          id: 2
-        },
-        {
-          img_url: '/resources/images/1003.jpg',
-          id: 3
-        },
-        {
-          img_url: '/resources/images/1004.jpg',
-          id: 4
-        },
-        {
-          img_url: '/resources/images/1005.jpg',
-          id: 5
-        },
-        {
-          img_url: '/resources/images/1006.jpg',
-          id: 6
-        }
-      ]
+      title: '',
+      icon: '',
+      list: []
     },
-    new_goods_list: [
-      {
-        img_url: '/resources/images/1001.png',
-        id: 1,
-        tags: [
-          {tagName: '套装', tag_id: 1},
-          {tagName: '包包', tag_id: 2},
-          {tagName: '蔻驰', tag_id: 3}
-        ],
-        price: 2500.00,
-        likes: 33
-      },
-      {
-        img_url: '/resources/images/1002.png',
-        id: 2,
-        tags: [
-          {tagName: '礼盒', tag_id: 1},
-          {tagName: '兰蔻套装', tag_id: 2},
-          {tagName: '送礼', tag_id: 3},
-          {tagName: '高端产品', tag_id: 4},
-          {tagName: '透明水晶', tag_id: 5},
-          {tagName: '晶莹剔透', tag_id: 6}
-        ],
-        price: 500.00,
-        likes: 2
-      },
-      {
-        img_url: '/resources/images/1001.png',
-        id: 3,
-        tags: [],
-        price: 2500.00,
-        likes: 133
-      }
-    ]
+    new_goods_list: []
   },
   onShow() {
     console.log('se')
+  },
+  onReady() {
+    this.init()
+  },
+  init() {
+    this.initSwiper();
+    this.initReco();
+    this.initLastGoods();
+  },
+  initLastGoods() {
+    my.request({
+      url: 'https://www.fastmock.site/mock/8a622a6c663c4636a86910b0eef8cbe5/api/index/list/last',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        let {data, status} = result.data;
+        if (status == 200) {
+          this.setData({
+            new_goods_list: data
+          })
+        }
+      }
+    });
+  },
+  initReco() {
+    my.request({
+      url: 'https://www.fastmock.site/mock/8a622a6c663c4636a86910b0eef8cbe5/api/index/list/reco',
+      method: 'get',
+      dataType: 'json',
+      success: (result) => {
+        let { data, status } = result.data;
+        if (status == 200) {
+          this.setData({
+            reco_goods: {...data}
+          });
+        }
+      }
+    });
+  },
+  initSwiper() {
+    my.request({
+      url: 'https://www.fastmock.site/mock/8a622a6c663c4636a86910b0eef8cbe5/api/index/carousels',
+      method: 'get',
+      data: {},
+      dataType: 'json',
+      success: (result) => {
+        let { data, status } = result.data;
+        if (status == 200) {
+          this.setData({
+            swiperList: data
+          })
+        }
+      }
+    });
   }
 });
